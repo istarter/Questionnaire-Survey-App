ca6fea31fe182bd1171ea08faee6abd80a85361f
Najib <naxibkhan@gmail.com>
2020-01-23 10:53:55 +0500

    issue fixed

--- app/Questionnaire.php
+++ app/Questionnaire.php
@@ -4,7 +4,10 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Questionnaire extends Model
-{
+class Questionnaire extends Model {
     protected $guarded = [];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
 }
